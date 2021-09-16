6704b7104bc74af22edb51b8be563040dc5156e0
sunny danu <mail@sunnydanu.com>
2021-09-16 15:04:54 +0530

    inter connection signup and sign in done

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,18 +1,49 @@
+import { useState, useEffect } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/request';
+
 const signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password
+    },
+    onSuccess: () => Router.push('/')
+  });
+
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    await doRequest();
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input className="form-control" />
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className="form-control"
+        />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control" />
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
       </div>
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
 };
-
-export default signup;
+export default signup
