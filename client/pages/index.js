6acb0f47609b403ad705af2610dbd758694e4f8d
sunny danu <mail@sunnydanu.com>
2021-09-16 15:31:42 +0530

    landing page done

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,32 +1,13 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  // console.log(currentUser);
-  // axios.get('/api/users/currentuser');
-  console.log(currentUser);
-
-  return <h1>Landing Page</h1>;
+  return currentUser ? (<h1>You are signed In</h1>) : (<h1>You are not signed in</h1>)
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // we are on the server!
-    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
+LandingPage.getInitialProps = async context => {
 
-    return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-
-    return data;
-  }
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;
