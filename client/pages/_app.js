22f8cf76b63dc40c808d3e0fd91ba1c18d123db4
sunny danu <mail@sunnydanu.com>
2021-09-16 16:03:03 +0530

    _app.js updated, share getInitalProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return <div>
+    <h1>Welcome {currentUser.email}</h1>
+    <Component {...pageProps} />
+  </div>
 };
 
-export default App;
+AppComponent.getInitialProps = async appContext => {
+
+  const { data } = await buildClient(appContext.ctx).get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return {
+    pageProps,
+    ...data
+  };
+};
+
+
+export default AppComponent;
