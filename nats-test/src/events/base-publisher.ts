e7c83c10cbe7c221e4a995c2b48bb1d9195d8d49
sunny danu <mail@sunnydanu.com>
2021-09-21 12:06:23 +0530

    nats-demo-updated

---
+++ nats-test/src/events/base-publisher.ts
@@ -0,0 +1,35 @@
+import { Stan } from 'node-nats-streaming';
+import { Subjects } from './subjects';
+
+interface Event {
+    subject: Subjects;
+    data: any;
+}
+
+export abstract class Publisher<T extends Event>{
+
+
+    private client: Stan;
+    readonly abstract subject: T['subject'];
+
+
+    constructor(client: Stan) {
+        this.client = client;
+    }
+
+    publish(data: T['data']) {
+
+        return new Promise<void>((resolve, reject) => {
+            this.client.publish(this.subject, JSON.stringify(data), (err) => {
+                if (err) {
+                    reject(err);
+                }
+                console.log(`Event Published: ${this.subject}`);
+                resolve();
+            });
+
+        })
+
+    }
+
+}
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,16 +1,24 @@
 import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 console.clear();
 const stan = nats.connect('ticketing', `publisher-${randomBytes(4).toString('hex')}`, {
     url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS server');
 
-    const data = JSON.stringify({
-        id: 123,
-        title: 'concert'
-    });
-    stan.publish('ticket:created', data, () => console.log('Event Published'));
+    const publisher = new TicketCreatedPublisher(stan);
+
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 20
+        })
+    } catch (err) {
+        console.error(err);
+    }
+
 })
